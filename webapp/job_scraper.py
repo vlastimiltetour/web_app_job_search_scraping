ce2e05ea29b26bb681fe977d263ca328fc8db705
vlastimil.tetour <v.tetour@gmail.com>
2022-08-19 12:28:28 +0200

    fourth update

--- webapp/job_scraper.py
+++ webapp/job_scraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def scrape_data(job_search):
-    global data
+    #global data
     df = pd.DataFrame(columns=['title', 'company', 'salary', 'link'])
 
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
--- webapp/main.py
+++ webapp/main.py
@@ -10,15 +10,15 @@ def home():
 
 @app.route('/scrape', methods=['POST', 'GET'])
 def results():
-    if request.method == 'GET':
+    if request.method == 'POST':
         job_search = request.form.get('job_search')
-        scrape_data(job_search)
+        fresh_data = scrape_data(job_search)
 
-        return render_template('home.html', data=data)
+        return render_template('home.html', job_results=fresh_data)
     else:
         print('ERROR')
 
-    return render_template('home.html', data=data)
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
